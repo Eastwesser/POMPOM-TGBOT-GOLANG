1ba0cb86451ed168191d9f67532df9517eb11afe
Dennis Mattews <alttert@mail.ru>
2025-01-26 23:46:31 +0300

    models upd

--- internal/models/order.go
+++ internal/models/order.go
@@ -6,3 +6,11 @@ package models
 	Модели базы данных для работы с PostgreSQL:
 		order.go: Модель для заказов (id, пользователь, товар, количество, статус).
 */
+
+type Order struct {
+	ID       int
+	UserID   int64
+	Product  Product
+	Quantity int
+	Status   string
+}
--- internal/models/subscriber.go
+++ internal/models/subscriber.go
@@ -6,3 +6,9 @@ package models
 	Модели базы данных для работы с PostgreSQL:
 		subscriber.go: Модель для подписчиков (id, telegram_id, дата подписки).
 */
+
+type Subscriber struct {
+	ID         int
+	TelegramID int64
+	Subscribed bool
+}
